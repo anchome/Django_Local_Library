0c427616bb8cee2aba8b2f82ac84872e873d0df4
anchome <casaalex.navarro@gmail.com>
2023-03-14 22:34:24 +0100

    2 commint

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7+ufy7e9+=t(%y^%4^705glaminzr4v*pcn-_n%g!v30u$me1_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
